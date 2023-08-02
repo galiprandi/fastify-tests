9ccf016ba1c644d7e5d2598282c70be3b3d2786b
German Aliporandi <german@egg.live>
2023-08-02 00:44:43 -0300

    Fix console message

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { appName, build } from './infra/server/server'
 
 const { PORT = '8000', HOST = '0.0.0.0' } = process.env
 
-const listenTextResolver = (address: string) => `${appName} Listening at ${address}/check`
+const listenTextResolver = (address: string) => `${appName} Listening at ${address}/api/check`
 
 ;(function init() {
   const app = build()
