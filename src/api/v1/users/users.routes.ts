5dd550aedd681962c0945908635fc895b8c368a0
German Aliporandi <german@egg.live>
2023-08-02 01:19:36 -0300

    Add random response to /v1/users

--- src/api/v1/users/users.routes.ts
+++ src/api/v1/users/users.routes.ts
@@ -1,12 +1,23 @@
 import { type FastifyServerOptions, type FastifyInstance, type RouteHandlerMethod } from 'fastify'
+import { faker } from '@faker-js/faker'
 
 export default async function (app: FastifyInstance, options: FastifyServerOptions) {
   app.get('', rootHandler)
 }
 
 const rootHandler: RouteHandlerMethod = async () => ({
-  users: [
-    { id: 1, name: 'John', lastName: 'Doe' },
-    { id: 2, name: 'Jane', lastName: 'Doe' },
-  ],
+  users,
 })
+
+const getRandomUser = () => ({
+  id: faker.datatype.uuid(),
+  name: faker.name.firstName(),
+  lastName: faker.name.lastName(),
+  phone: faker.phone.number(),
+  email: faker.internet.email(),
+  avatar: faker.image.avatar(),
+  city: faker.address.city(),
+  country: faker.address.country(),
+})
+
+const users = faker.helpers.multiple(getRandomUser, { count: 10 })
