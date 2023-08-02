a14c9d1109ab4f00a290cd49bfcdffd56e6e3cb0
German Aliporandi <german@egg.live>
2023-08-02 00:55:36 -0300

    Add schema to /check

--- src/api/check/check.routes.ts
+++ src/api/check/check.routes.ts
@@ -1,7 +1,20 @@
-import { type FastifyServerOptions, type FastifyInstance, type RouteHandlerMethod } from 'fastify'
+import { type FastifyServerOptions, type FastifyInstance, type RouteHandlerMethod, type FastifySchema } from 'fastify'
 
 export default async function (app: FastifyInstance, options: FastifyServerOptions) {
-  app.get('', rootHandler)
+  app.get('', { schema: rootGetSchema }, rootHandler)
 }
 
+// Move to controllers folder
 const rootHandler: RouteHandlerMethod = async (request) => ({ status: 'ok' })
+
+// Move to schemas folder
+const rootGetSchema: FastifySchema = {
+  response: {
+    200: {
+      type: 'object',
+      properties: {
+        status: { type: 'string' },
+      },
+    },
+  },
+}
