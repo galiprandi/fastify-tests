450c46eeb18aa7300a90694277f3c61129adbfd2
German Aliporandi <german@egg.live>
2023-08-02 01:05:43 -0300

    Improve CORS plugin

--- src/infra/server/plugins/cors.plugin.ts
+++ src/infra/server/plugins/cors.plugin.ts
@@ -1,13 +1,10 @@
-import { type FastifyPluginAsync } from 'fastify'
+import { type FastifyInstance } from 'fastify'
 import cors from '@fastify/cors'
 import { appName } from '../server'
 
 const name = 'CORS'
 
-const corsPlugin: FastifyPluginAsync = async (app) => {
+export default async (app: FastifyInstance) =>
   app.register(cors, { origin: '*' }).ready((err) => {
     err ? app.log.fatal(err, `${appName} ${name} Plugin failed!`) : app.log.info(`${appName} ${name} Plugin ready!`)
   })
-}
-
-export default corsPlugin
