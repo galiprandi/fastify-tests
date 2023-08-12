8fbc952785a469949d68adc7c39215f592ea8294
German Aliporandi <german@egg.live>
2023-08-12 00:53:04 -0300

    server.ts refactor

--- src/infra/server/server.ts
+++ src/infra/server/server.ts
@@ -1,13 +1,13 @@
+import Fastify from 'fastify'
 import { envToLogger, environment } from './logger'
 import { type JsonSchemaToTsProvider } from '@fastify/type-provider-json-schema-to-ts'
 import AutoLoad from '@fastify/autoload'
 import Env from '@fastify/env'
-import Cors from '@fastify/cors'
-import Helmet from '@fastify/helmet'
-import Fastify from 'fastify'
-import { join } from 'node:path'
 import { EnvOptions } from './config/env'
+import { join } from 'node:path'
+import Cors from '@fastify/cors'
 import { CorsOptions } from './config/cors'
+import Helmet from '@fastify/helmet'
 import { HelmetOptions } from './config/helmet'
 
 export const appName = '🚀 APP:'
@@ -18,26 +18,24 @@ export async function build() {
     logger: envToLogger[environment] ?? true,
   }).withTypeProvider<JsonSchemaToTsProvider>()
 
-  await srv.register(Env, EnvOptions)
-  await srv.register(Cors, CorsOptions)
-  await srv.register(Helmet, HelmetOptions)
-
-  srv.env.DEBUG
-    ? srv.log.info(`\n┌── ${appName} Plugins\n${srv.printPlugins()}`)
-    : srv.log.info(`${appName} Plugins ready!`)
-
-  await srv.register(AutoLoad, {
-    dir: join(__dirname, '../../api'),
-    options: { prefix: '/api' },
-    ignorePattern: /.*(test|spec)\.ts/,
-  })
-
-  srv.env.DEBUG
-    ? srv.log.info(`\n┌── ${appName} Routes\n${srv.printRoutes({ commonPrefix: false })}`)
-    : srv.log.info(`${appName} Routes ready!`)
+  await srv
+    .register(Env, EnvOptions)
+    .register(Cors, CorsOptions)
+    .register(Helmet, HelmetOptions)
+    .register(AutoLoad, {
+      dir: join(__dirname, '../../api'),
+      options: { prefix: '/api' },
+      ignorePattern: /.*(test|spec)\.ts/,
+    })
 
   srv.ready(() => {
-    srv.env.DEBUG && srv.log.info('\n', srv.printRoutes())
+    if (srv.env.DEBUG) {
+      srv.log.info(`\n┌── ${appName} Plugins\n${srv.printPlugins()}`)
+      srv.log.info(`\n┌── ${appName} Routes\n${srv.printRoutes({ commonPrefix: false })}`)
+    } else {
+      srv.log.info(`${appName} Plugins ready!`)
+      srv.log.info(`${appName} Routes ready!`)
+    }
   })
 
   return srv
